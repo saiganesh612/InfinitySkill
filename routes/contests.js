0097da64f86bbc74702728011d70e5e7ce2e4113
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2021-06-09 00:44:38 +0530

    add functionalities to admin

---
+++ routes/contests.js
@@ -0,0 +1,58 @@
+const express = require("express")
+const router = express.Router();
+const User = require("../models/user")
+const Contest = require("../models/contest")
+const { transporter, sender } = require("../settings")
+
+router.get("/requested-contests", async (req, res) => {
+    try {
+        const requestedContests = await Contest.find({ isApproved: { $eq: false } })
+        res.render("contests/requested", { page: "Requested Contests", requestedContests })
+    } catch (err) {
+        console.log(err)
+        res.redirect("/home")
+    }
+})
+
+router.get("/approved-contests", async (req, res) => {
+    try {
+        const approvedContests = await Contest.find({ isApproved: { $eq: true } })
+        res.render("contests/approved", { page: "Approved Contests", approvedContests: approvedContests.reverse() })
+    } catch (err) {
+        console.log(err)
+        res.redirect("/home")
+    }
+})
+
+router.get("/approve/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const contest = await Contest.findByIdAndUpdate(id, { isApproved: true })
+        const user = await User.findOne({ username: { $eq: contest.owner } })
+
+        const body = `
+            Hey ${contest.owner},
+            Admin reviewed your ${contest.contestName} contest and was approved by the admin.
+            Now, you can proceed with the contest.
+        `
+
+        const mailOptions = {
+            from: sender,
+            to: user.email,
+            subject: `Status of ${contest.contestName} contest.`,
+            text: body
+        }
+
+        transporter.sendMail(mailOptions, err => {
+            if (err) return console.log("error occured", err)
+            return console.log('mail sent');
+        })
+
+        res.redirect("/requested-contests")
+    } catch (err) {
+        console.log(err)
+        res.redirect("/requested-contests")
+    }
+})
+
+module.exports = router
