a01251e1f06fd5fcefa4b80cf3c33feb9b97ee91
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2021-06-10 18:27:29 +0530

    add new route and page for every contest

---
+++ public/assets/js/postContest.js
@@ -0,0 +1,104 @@
+function myFunction() {
+    var x = document.getElementById("mySelect").value;
+    if (x == "Dancing") {
+        document.getElementById("1").innerHTML = "Indian dances"
+        document.getElementById("2").innerHTML = "Bachata"
+        document.getElementById("3").innerHTML = "Hip-hop"
+        document.getElementById("4").innerHTML = "Salsa"
+        document.getElementById("5").innerHTML = "Ballet"
+        document.getElementById("6").innerHTML = "Strip"
+        document.getElementById("7").innerHTML = "Rock n roll"
+        document.getElementById("8").innerHTML = "Break dance"
+    }
+    if (x == "Singing") {
+        document.getElementById("1").innerHTML = "Classical"
+        document.getElementById("2").innerHTML = "Traditional"
+        document.getElementById("3").innerHTML = "Folk"
+        document.getElementById("4").innerHTML = "Filmi"
+        document.getElementById("5").innerHTML = "Rock"
+        document.getElementById("6").innerHTML = "Pop"
+        document.getElementById("7").innerHTML = "Religions"
+    }
+    if (x == "Music") {
+        document.getElementById("1").innerHTML = "Accordion"
+        document.getElementById("2").innerHTML = "Banjo"
+        document.getElementById("3").innerHTML = "Bass guitar"
+        document.getElementById("4").innerHTML = "Drum"
+        document.getElementById("5").innerHTML = "Tabla"
+    }
+    if (x == "Art") {
+        document.getElementById("1").innerHTML = "Painting"
+        document.getElementById("2").innerHTML = " Sculpture"
+        document.getElementById("3").innerHTML = "Cubism"
+        document.getElementById("4").innerHTML = "Drawing"
+        document.getElementById("5").innerHTML = " Prints"
+        document.getElementById("6").innerHTML = "Crafts"
+        document.getElementById("7").innerHTML = "Design"
+        document.getElementById("8").innerHTML = "Performance"
+        document.getElementById("9").innerHTML = "Decoration"
+        document.getElementById("10").innerHTML = "Entertainment"
+        document.getElementById("11").innerHTML = "Calligraphy"
+    }
+    if (x == "Graphic design") {
+        document.getElementById("1").innerHTML = "Visual identity"
+        document.getElementById("2").innerHTML = "Marketing and advertising"
+        document.getElementById("3").innerHTML = "UI"
+        document.getElementById("4").innerHTML = "Publication"
+        document.getElementById("5").innerHTML = " Packaging"
+        document.getElementById("6").innerHTML = "Motion Graphics"
+        document.getElementById("7").innerHTML = "Art and Illustration"
+        document.getElementById("8").innerHTML = " Web design"
+        document.getElementById("9").innerHTML = "App design"
+    }
+    if (x == "reels") {
+        document.getElementById("1").innerHTML = "Comedy"
+        document.getElementById("2").innerHTML = "Shayari"
+        document.getElementById("3").innerHTML = "Poetry"
+        document.getElementById("4").innerHTML = "Motivation speech"
+        document.getElementById("5").innerHTML = " Advertisement"
+        document.getElementById("6").innerHTML = "Dance"
+        document.getElementById("7").innerHTML = "Mimicry "
+        document.getElementById("8").innerHTML = " Dialogue"
+    }
+    if (x == "VideoEditing") {
+        document.getElementById("1").innerHTML = "Green screen"
+        document.getElementById("2").innerHTML = "Video montage"
+        document.getElementById("3").innerHTML = "Documentary"
+        document.getElementById("4").innerHTML = "Music video"
+        document.getElementById("5").innerHTML = "Short film"
+        document.getElementById("6").innerHTML = "Wedding"
+        document.getElementById("7").innerHTML = "Debut "
+        document.getElementById("8").innerHTML = " Vlogs"
+        document.getElementById("9").innerHTML = "Presentation"
+
+    }
+    if (x == "PhotoEditing") {
+        document.getElementById("1").innerHTML = " Clean and natural"
+        document.getElementById("2").innerHTML = "Matte look"
+        document.getElementById("3").innerHTML = "High contrast"
+        document.getElementById("4").innerHTML = "Classic"
+        document.getElementById("5").innerHTML = "Black and white"
+        document.getElementById("6").innerHTML = "Monochrome"
+        document.getElementById("7").innerHTML = "HDR "
+        document.getElementById("8").innerHTML = " Cross processed"
+    }
+    if (x == "SoftSkills") {
+        document.getElementById("1").innerHTML = " Power point"
+        document.getElementById("2").innerHTML = "Excel"
+        document.getElementById("3").innerHTML = "AutoCAD"
+        document.getElementById("4").innerHTML = "Word"
+        document.getElementById("5").innerHTML = "Article writing"
+    }
+    if (x == "Photography") {
+        document.getElementById("1").innerHTML = "Portrait "
+        document.getElementById("2").innerHTML = "Journalism"
+        document.getElementById("3").innerHTML = "Fashion"
+        document.getElementById("4").innerHTML = "Sport"
+        document.getElementById("5").innerHTML = "Still life"
+        document.getElementById("6").innerHTML = "Editorial"
+        document.getElementById("7").innerHTML = "Architecture"
+        document.getElementById("8").innerHTML = "Wedding"
+        document.getElementById("9").innerHTML = "Nature"
+        document.getElementById("10").innerHTML = "Event"
+    }
+}
