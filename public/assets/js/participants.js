20eebccd6441e4085e1dd05659ec94230ab08591
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2021-07-02 23:44:03 +0530

    changed voting system for admin

--- public/assets/js/participants.js
+++ public/assets/js/participants.js
@@ -1,10 +1,29 @@
-const calculatePoints = (participants, currentUser, owner, index) => {
+const handleSubmit = () => {
+    const points = document.getElementById("adminPoints").value
+    const pname = document.getElementById("pn").value
+
+    if (!pname || !points) {
+        alert("Enter all fields...")
+        return
+    }
+
+    const index = participantsData.findIndex(p => p.user === pname)
+    if (index === -1) {
+        alert("User not found. Please check spelling and try again.")
+        return
+    }
+    document.getElementById("pn").value = ""
+    document.getElementById("adminPoints").value = ""
+    calculatePoints(participantsData, userObj, contestOwner, index + 1, parseInt(points))
+}
+
+const calculatePoints = (participants, currentUser, owner, index, adminPoints = 0) => {
     const { _id } = window.contest
     const points = document.getElementById(parseInt(index))
     const btns = document.getElementsByClassName(parseInt(index))
     let tempPoints
 
-    if (currentUser.username === "Admin") tempPoints = parseInt(points.innerText) + 50
+    if (currentUser.username === "Admin" && !isNaN(adminPoints)) tempPoints = parseInt(points.innerText) + adminPoints
     else if (currentUser.username === owner) tempPoints = parseInt(points.innerText) + 10
     else if (participants.filter(p => p.user === currentUser.username).length) tempPoints = parseInt(points.innerText) + 5
     else tempPoints = parseInt(points.innerText) + 1
